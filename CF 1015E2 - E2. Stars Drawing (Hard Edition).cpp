a7ba0fbccd9be39b499119a41fe86a17dce76257
MTB Ishmam <134937835+mtbishmam@users.noreply.github.com>
2024-08-21 11:32:37 +0600

    Update CF 1015E2 - E2. Stars Drawing (Hard Edition).cpp

--- CF 1015E2 - E2. Stars Drawing (Hard Edition).cpp
+++ CF 1015E2 - E2. Stars Drawing (Hard Edition).cpp
@@ -1,1 +1,123 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <queue>
+#include <bitset>
+#include <algorithm>
+#include <numeric>
+#include <math.h>
+#include <iomanip>
+#include <cstring>
+// #include <cassert>
+#include <functional>
+using namespace std;
 
+#define endl "\n"
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define lb lower_bound
+#define ub upper_bound
+#define em emplace
+
+template <typename T> istream& operator>>(istream& is, vector<T>& a) { for (auto& i : a) is >> i; return is; }
+template <typename T> ostream& operator<<(ostream& os, vector<T>& a) { for (auto& i : a) os << i << " "; return os; };
+template <typename T> ostream& operator<<(ostream& os, set<T>& s) { for (auto i : s) os << i << " "; return os; }
+// template <typename T> ostream& operator<<(ostream &os, const vector<T>& v) { os << '{'; string s; for (const auto &x : v) os << s << x, s = " "; return os << '}'; }
+template <typename A, typename B> istream& operator>>(istream& is, pair<A, B>& i) { return is >> i.ff >> i.ss; }
+template <typename A, typename B> ostream& operator<<(ostream& os, const pair<A, B>& i) { return os << i.ff << " " << i.ss; }
+// template <typename A, typename B> ostream& operator<<(ostream& os, const pair<A, B>& i) { return os << '{' << i.ff << ", " << i.ss << '}'; }
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#define debug(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+using ll    = long long      ; using ld    = long double  ; using ull = unsigned ll    ;
+using pii   = pair<int, int> ; using pli  = pair<ll, int> ; using pil = pair<int, ll>  ;
+using pll   = pair<ll, ll>   ; using vl   = vector<ll>    ; using vvl = vector<vl>     ;
+using vi    = vector<int>    ; using vb   = vector<bool>  ; using vc  = vector<char>   ;
+using vvi   = vector<vi>     ; using vvb  = vector<vb>    ; using vvc = vector<vc>     ;
+using vpii  = vector<pii>    ; using vpll = vector<pll>   ; using vs  = vector<string> ;
+using vvpii = vector<vpii>   ; using vvvi = vector<vvi>   ; using vvvl = vector<vvl>   ;
+using vull = vector<ull>     ;
+using tiii = tuple<int, int, int>;                        ; using vtiii = vector<tiii> ;
+template <class A, class B> using umap = unordered_map<A, B>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define bug cerr << "!Bugged..." << endl
+#define add(x, y) (x + y >= MOD ? x + y - MOD : x + y)
+#define sz(x) (int)(x).size()
+
+const string cq[2] = {"NO", "YES"};
+const int dx[8] = { -1,  0, 0, 1, 1,  1, -1, -1};
+const int dy[8] = { 0, -1, 1, 0, 1, -1,  1, -1};
+const int INF = 2147483647;
+const ll LINF = 9223372036854775807;
+const int MOD = 1e9 + 7;
+const int maxn = 1e5 + 1;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // freopen("error.txt", "a", stderr);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    // cout.tie(NULL);
+
+    int T(1);
+    // cin >> T;
+    for (int Ti = 1; Ti <= T; Ti++)
+    {
+        int n, m;
+        cin >> n >> m;
+        vvc a(n, vc(m)), b(n, vc(m, '.'));
+        for (auto& i : a) cin >> i;
+        // for (auto& i : a) debug(i);
+
+        auto valid = [&](int x, int y) {
+            bool ok = (0 <= x and x < n and 0 <= y and y < m);
+            if (!ok) return ok;
+            ok &= a[x][y] == '*';
+            return ok;
+        };
+        vvb vis(n, vb(m));
+        vtiii ans;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (a[i][j] == '*') {
+                    // if (vis[i][j]) continue;
+                    int x1 = i - 1, y1 = j;
+                    int x2 = i + 1, y2 = j;
+                    int x3 = i, y3 = j - 1;
+                    int x4 = i, y4 = j + 1;
+                    int len = 0;
+                    while (valid(x1, y1) and valid(x2, y2) and valid(x3, y3) and valid(x4, y4)) {
+                        b[x1][y1] = b[x2][y2] = b[x3][y3] = b[x4][y4] = '*';
+                        len++;
+                        x1--;
+                        x2++;
+                        y3--;
+                        y4++;
+                    }
+                    if (len) {
+                        b[i][j] = '*';
+                        ans.eb(i, j, len);
+                    }
+                }
+            }
+        }
+        // for (auto& i : b) debug(i);
+        if (b == a) {
+            cout << sz(ans) << endl;
+            for (auto&[x, y, len] : ans)
+                cout << x + 1 << " " << y + 1 << " " << len << endl;
+        } else cout << -1;
+    }
+    return 0;
+}
