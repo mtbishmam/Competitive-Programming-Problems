34df4bac5747d998627c17550fb8fca360899484
MTB Ishmam <134937835+mtbishmam@users.noreply.github.com>
2024-09-08 01:14:13 +0600

    Update CF 1329A - A. Dreamoon Likes Coloring.cpp

--- CF 1329A - A. Dreamoon Likes Coloring.cpp
+++ CF 1329A - A. Dreamoon Likes Coloring.cpp
@@ -1,1 +1,114 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <queue>
+#include <bitset>
+#include <algorithm>
+#include <numeric>
+#include <math.h>
+#include <iomanip>
+#include <cstring>
+using namespace std;
 
+#define endl "\n"
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define lb lower_bound
+#define ub upper_bound
+#define em emplace
+
+template <typename T> istream& operator>>(istream& is, vector<T>& a) { for (auto& i : a) is >> i; return is; }
+template <typename T> ostream& operator<<(ostream& os, vector<T>& a) { for (auto& i : a) os << i << " "; return os; };
+// template <typename T> ostream& operator<<(ostream &os, const vector<T>& v) { os << '{'; string s; for (const auto &x : v) os << s << x, s = " "; return os << '}'; }
+template <typename A, typename B> istream& operator>>(istream& is, pair<A, B>& i) { return is >> i.ff >> i.ss; }
+template <typename A, typename B> ostream& operator<<(ostream& os, const pair<A, B>& i) { return os << i.ff << " " << i.ss; }
+// template <typename A, typename B> ostream& operator<<(ostream& os, const pair<A, B>& i) { return os << '{' << i.ff << ", " << i.ss << '}'; }
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#define debug(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+using ll    = long long;
+using ld    = long double;
+using pii   = pair<int, int>; using pli  = pair<ll, int>; using pil = pair<int, ll>;
+using pll   = pair<ll, ll>  ; using vl   = vector<ll>   ; using vvl = vector<vl>;
+using vi    = vector<int>   ; using vb   = vector<bool> ; using vc  = vector<char>;
+using vvi   = vector<vi>    ; using vvb  = vector<vb>   ; using vvc = vector<vc>;
+using vpii  = vector<pii>   ; using vpll = vector<pll>  ; using vs  = vector<string>;
+using vvpii = vector<vpii>  ; using vvvi = vector<vvi>  ; using vvvl = vector<vvl>;
+using tiii = tuple<int, int, int>;                      ; using vtiii = vector<tiii>;
+template <class A, class B> using umap = unordered_map<A, B>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define add(x, y) (x + y >= MOD ? x + y - MOD : x + y)
+
+const string cq[2] = {"NO", "YES"};
+const int dx[8] = { -1,  0, 0, 1, 1,  1, -1, -1};
+const int dy[8] = { 0, -1, 1, 0, 1, -1,  1, -1};
+const int INF = 2147483647;
+const ll LINF = 9223372036854775807;
+const int MOD = 1e9 + 7;
+const int maxn = 1e5 + 1;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // freopen("error.txt", "a", stderr);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    // cout.tie(NULL);
+
+    int T(1);
+    // cin >> T;
+    for (int Ti = 1; Ti <= T; Ti++)
+    {
+        int m, n;
+        cin >> m >> n;
+        vi a(n), ans(n); cin >> a;
+        vl suf(n + 2);
+        for (int i = n - 1; i >= 0; i--)
+            suf[i] = suf[i + 1] + a[i];
+        bool ok = 1;
+        if (suf[0] < n) ok = 0;
+        else {
+            vi b(m); iota(all(b), 1);
+            reverse(all(b));
+            // cout << b << endl;
+            for (int i = 0; i < min(n, m); i++) {
+                if (a[i] > b[i]) ok = 0;
+            }
+            // debug(ok);
+            vl pre(m + 1); int col = 1;
+            for (int i = 1, j = 0; i <= m; i++) {
+                if (j < n and (i + a[j] - 1) + suf[j + 1] >= m) {
+                    pre[i] = col++;
+                    ans[j] = i;
+                    j++;
+                }
+            }
+            int prev = 0;
+            for (int i = 1; i <= m; ++i) {
+                if (!pre[i]) pre[i] = pre[i - 1];
+            }
+
+            set<int> s;
+            for (int i = 1; i <= m; ++i) {
+                if (pre[i] == 0) ok = 0;
+                s.em(pre[i]);
+            }
+            if (s.size() != n) ok = 0;
+        }
+        if (ok) {
+            cout << ans;
+        }
+        else cout << -1;
+    }
+    return 0;
+}
