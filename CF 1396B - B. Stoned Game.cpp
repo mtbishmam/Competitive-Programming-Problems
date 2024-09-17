357ccb93123e149e2b5104a79bb435f68e0d4d83
MTB Ishmam <134937835+mtbishmam@users.noreply.github.com>
2024-09-17 08:29:04 +0600

    Update CF 1396B - B. Stoned Game.cpp

--- CF 1396B - B. Stoned Game.cpp
+++ CF 1396B - B. Stoned Game.cpp
@@ -8,12 +8,14 @@ int32_t main() {
 	while (t--) {
 		int n; cin >> n;
 		vector<int> a(n);
-		for (int i = 0; i < n; i++) cin >> a[i];
+		int mx = 0;
+		for (int i = 0; i < n; i++) cin >> a[i], mx = max(mx, a[i]);
 		int sum = 0;
 		for (int i = 0; i < n; i++) sum += a[i];
 		int win = 0;
 		if (n == 1) win = 1;
 		else if (n == 2) win = a[0] != a[1];
+		else if (mx > sum - mx) win = 1;
 		else win = sum % 2;
 		if (win) cout << "T\n";
 		else cout << "HL\n";
