76f07c4d8f4c713c5b2f2070fde797c64941407c
MTB Ishmam <134937835+mtbishmam@users.noreply.github.com>
2024-09-19 19:00:02 +0600

    Update CF 478C - C. Table Decorations.cpp

--- CF 478C - C. Table Decorations.cpp
+++ CF 478C - C. Table Decorations.cpp
@@ -19,9 +19,9 @@ using namespace std;
 #define ss second
 #define lb lower_bound
 #define ub upper_bound
-#define e emplace
+#define em emplace
 
-template <typename T> istream& operator>>(istream &is, vector<T>& a) { for (auto& i : a) is >> i; return is; }
+template <typename T> istream& operator>>(istream& is, vector<T>& a) { for (auto& i : a) is >> i; return is; }
 template <typename T> ostream& operator<<(ostream& os, vector<T>& a) { for (auto& i : a) os << i << " "; return os; };
 // template <typename T> ostream& operator<<(ostream &os, const vector<T>& v) { os << '{'; string s; for (const auto &x : v) os << s << x, s = " "; return os << '}'; }
 template <typename A, typename B> istream& operator>>(istream& is, pair<A, B>& i) { return is >> i.ff >> i.ss; }
@@ -33,13 +33,13 @@ template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cer
 
 using ll    = long long;
 using ld    = long double;
-using pii   = pair<int, int>; using pli  = pair<ll, int>;  using pil = pair<int, ll>;
-using pll   = pair<ll, ll>;   using vl   = vector<ll>;     using vvl = vector<vl>;
-using vi    = vector<int>;    using vb   = vector<bool>;   using vc  = vector<char>;
-using vvi   = vector<vi>;     using vvb  = vector<vb>;     using vvc = vector<vc>;
-using vpii  = vector<pii>;    using vpll = vector<pll>;    using vs  = vector<string>;
-using vvpii = vector<vpii>;   using tiii = tuple<int, int, int>;
-using vtiii = vector<tiii>;
+using pii   = pair<int, int>; using pli  = pair<ll, int>; using pil = pair<int, ll>;
+using pll   = pair<ll, ll>  ; using vl   = vector<ll>   ; using vvl = vector<vl>;
+using vi    = vector<int>   ; using vb   = vector<bool> ; using vc  = vector<char>;
+using vvi   = vector<vi>    ; using vvb  = vector<vb>   ; using vvc = vector<vc>;
+using vpii  = vector<pii>   ; using vpll = vector<pll>  ; using vs  = vector<string>;
+using vvpii = vector<vpii>  ; using tiii = tuple<int, int, int>;
+using vtiii = vector<tiii>  ;
 template <class A, class B> using umap = unordered_map<A, B>;
 
 #define all(x) (x).begin(), (x).end()
@@ -51,54 +51,64 @@ const int dy[8] = { 0, -1, 1, 0, 1, -1,  1, -1};
 const int INF = 2147483647;
 const ll LINF = 9223372036854775807;
 const int MOD = 1e9 + 7;
-const int maxn = 1e5 + 1;
+const int maxn = 50 + 1;
+int dp[maxn][maxn][maxn];
 
 int main()
 {
 #ifndef ONLINE_JUDGE
     // freopen("input.txt", "r", stdin);
     // freopen("output.txt", "w", stdout);
-    freopen("error.txt", "a", stderr);
+    // freopen("error.txt", "a", stderr);
 #endif
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
     // cout.tie(NULL);
 
-    int ttt(1);
-    // cin >> ttt;
-    for (int itt = 1; itt <= ttt; itt++)
+    int T(1);
+    // cin >> T;
+    for (int Ti = 1; Ti <= T; Ti++)
     {
-        ll r, g, b;
-        cin >> r >> g >> b;
-        ll mn = min(r, min(g, b));
-        ll ans = mn;
-        r -= mn;
-        g -= mn;
-        b -= mn;
-        vl pos;
-        if (r > 0) pos.eb(r);
-        if (g > 0) pos.eb(g);
-        if (b > 0) pos.eb(b);
+        int a, b, c;
+        cin >> a >> b >> c;
+        memset(dp, -1, sizeof dp);
+        function<int(int, int, int)> f = [&](int i, int j, int k) {
+            int& ret = dp[i][j][k];
+            if (~ret) return ret;
 
-        sort(all(pos));
-        if (pos.size() >= 2) {
-            ll l = 0, r = 2e9, best(0);
-            ll a = pos[0], b = pos[1];
-            while (l <= r) {
-                ll mid = l + r >> 1;
-                if (b - 2 * mid >= 0 and a - mid >= 0)
-                    l = mid + 1, best = mid;
-                else r = mid - 1;
-            }
-            ans += best;
-        }
+            ret = 0;
+            if (i >= 2 and j >= 1) ret = max(ret, 1 + f(i - 2, j - 1, k));
+            if (i >= 2 and k >= 1) ret = max(ret, 1 + f(i - 2, j, k - 1));
 
-        // mn = LINF; ll s(0);
-        // for (ll& i : pos) mn = min(mn, i), s += i;
-        // mn = min(mn, s / 3);
-        // ans += mn;
-        cout << ans;
+            if (j >= 2 and i >= 1) ret = max(ret, 1 + f(i - 1, j - 2, k));
+            if (j >= 2 and k >= 1) ret = max(ret, 1 + f(i, j - 2, k - 1));
 
+            if (k >= 2 and i >= 1) ret = max(ret, 1 + f(i - 1, j, k - 2));
+            if (k >= 2 and j >= 1) ret = max(ret, 1 + f(i, j - 1, k - 2));
+
+            if (i and j and k) ret = max(ret, 1 + f(i - 1, j - 1, k - 1));
+            return ret;
+        };
+        auto solve = [](int r, int g, int b) {
+            vi a = {r, g, b}; sort(all(a));
+            if (!a[0] and !a[1]) return 0;
+            if (!a[0]) return min((a[1] + a[2]) / 3, a[1]);
+            int ret = (r + g + b) / 3;
+            ret = min(ret, a[0] + a[1]);
+            return ret;
+            // return min({(r + g + b) / 3, a[2] / 2, a[0] + a[1]});
+        };
+        cout << solve(a, b, c) << endl;
+        // for (int i = 0; i < maxn; i++)
+        //     for (int j = 0; j < maxn; j++)
+        //         for (int k = 0; k < maxn; k++) {
+        //             int x = solve(i, j, k);
+        //             int y = f(i, j, k);
+        //             if (x != y) {
+        //                 cout << i << " " << j << " " << k << " -> " << x << " " << y << endl;
+        //             }
+        //             // cout << solve(i, j, k) << " " << f(i, j, k) << endl;
+        //         }
     }
     return 0;
 }
