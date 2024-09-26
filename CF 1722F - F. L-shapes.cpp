ddbb5cabad850a37138807715974840d14740473
MTB Ishmam <134937835+mtbishmam@users.noreply.github.com>
2024-09-26 11:20:07 +0600

    Update CF 1722F - F. L-shapes.cpp

--- CF 1722F - F. L-shapes.cpp
+++ CF 1722F - F. L-shapes.cpp
@@ -4,11 +4,14 @@
 #include <set>
 #include <unordered_map>
 #include <queue>
+#include <bitset>
 #include <algorithm>
 #include <numeric>
 #include <math.h>
 #include <iomanip>
-#include <cassert>
+#include <cstring>
+// #include <cassert>
+#include <functional>
 using namespace std;
 
 #define endl "\n"
@@ -18,27 +21,47 @@ using namespace std;
 #define ss second
 #define lb lower_bound
 #define ub upper_bound
+#define em emplace
 
-using ll = long long; using pii = pair<int, int>;
-using pll = pair<ll, ll>; using vs  = vector<string>;
-using vi = vector<int>; using vvi = vector<vi>;
-using vl = vector<ll>; using vvl = vector<vl>;
-using vb = vector<bool>; using vvb = vector<vb>;
-using vc  = vector<char>; using vvc = vector<vc>;
-using vpii  = vector<pii>; using vpll = vector<pll>;
+template <typename T> istream& operator>>(istream& is, vector<T>& a) { for (auto& i : a) is >> i; return is; }
+template <typename T> ostream& operator<<(ostream& os, vector<T>& a) { for (auto& i : a) os << i << " "; return os; };
+template <typename T> ostream& operator<<(ostream& os, set<T>& s) { for (auto i : s) os << i << " "; return os; }
+// template <typename T> ostream& operator<<(ostream &os, const vector<T>& v) { os << '{'; string s; for (const auto &x : v) os << s << x, s = " "; return os << '}'; }
+template <typename A, typename B> istream& operator>>(istream& is, pair<A, B>& i) { return is >> i.ff >> i.ss; }
+template <typename A, typename B> ostream& operator<<(ostream& os, const pair<A, B>& i) { return os << i.ff << " " << i.ss; }
+// template <typename A, typename B> ostream& operator<<(ostream& os, const pair<A, B>& i) { return os << '{' << i.ff << ", " << i.ss << '}'; }
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#define debug(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+using ll    = long long      ; using ld    = long double  ; using ull = unsigned ll    ;
+using pii   = pair<int, int> ; using pli  = pair<ll, int> ; using pil = pair<int, ll>  ;
+using pll   = pair<ll, ll>   ; using vl   = vector<ll>    ; using vvl = vector<vl>     ;
+using vi    = vector<int>    ; using vb   = vector<bool>  ; using vc  = vector<char>   ;
+using vvi   = vector<vi>     ; using vvb  = vector<vb>    ; using vvc = vector<vc>     ;
+using vpii  = vector<pii>    ; using vpll = vector<pll>   ; using vs  = vector<string> ;
+using vvpii = vector<vpii>   ; using vvvi = vector<vvi>   ; using vvvl = vector<vvl>   ;
+using vull = vector<ull>     ;
+using tiii = tuple<int, int, int>;                        ; using vtiii = vector<tiii> ;
+template <class A, class B> using umap = unordered_map<A, B>;
 
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
+#define bug cerr << "!Bugged..." << endl
+#define add(x, y) (x + y >= MOD ? x + y - MOD : x + y)
 #define sz(x) (int)(x).size()
 
 const string cq[2] = {"NO", "YES"};
+const int dx[8] = { -1,  0, 0, 1, 1,  1, -1, -1};
+const int dy[8] = { 0, -1, 1, 0, 1, -1,  1, -1};
+const int INF = 2147483647;
+const ll LINF = 9223372036854775807;
 const int MOD = 1e9 + 7;
 const int maxn = 1e5 + 1;
 
 int main()
 {
 #ifndef ONLINE_JUDGE
-#define bug(x) cerr << #x << endl
     // freopen("input.txt", "r", stdin);
     // freopen("output.txt", "w", stdout);
     // freopen("error.txt", "a", stderr);
@@ -48,35 +71,90 @@ int main()
     // cout.tie(NULL);
 
     int T(1);
-    // cin >> T;
+    cin >> T;
     for (int Ti = 1; Ti <= T; Ti++)
     {
-        int n; cin >> n;
-        ll k; cin >> k;
-        vl a(n);
+        int n, m;
+        cin >> n >> m;
+        vvc a(n, vc(m));
         for (auto& i : a) cin >> i;
-        sort(rall(a));
 
-        int l = 1, r = n, ans = -1;
-        auto chk = [&](int md) {
-            vi g[md];
-            for (int i = 0; i < n; i++) {
-                g[i % md].eb(a[i]);
-            }
-            ll cur = 0;
-            for (int i = 0; i < md; i++) {
-                for (int j = 0; j < sz(g[i]); j++)
-                    cur += max(0, g[i][j] - j);
-            }
-            return cur >= k;
+        vpii xxs;
+        vvb vis(n, vb(m));
+        auto valid = [&](int x, int y) {
+            return (0 <= x and x < n and 0 <= y and y < m);
         };
-        while (l <= r) {
-            int mid = l + r >> 1;
-            if (chk(mid)) ans = mid, r = mid - 1;
-            else l = mid + 1;
-        }
-        cout << ans;
+        auto chk = [&](vpii & cur) {
+            bool ok = 1;
+            set<pii> curse;
+            for (auto&[x, y] : cur) {
+                if (valid(x, y)) {
+                    ok &= (a[x][y] == '*');
+                }
+                else return false;
+                curse.em(x, y);
+            }
+            for (auto&[x, y] : cur) {
+                for (int i = 0; i < 8; i++) {
+                    int nx = x + dx[i];
+                    int ny = y + dy[i];
+                    if (valid(nx, ny) and !curse.count({nx, ny})) {
+                        ok &= a[nx][ny] == '.';
+                    }
+                }
+            }
+            return ok;
+        };
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (a[i][j] == '*') {
+                    if (vis[i][j]) continue;
+                    xxs = {{i, j}, {i + 1, j}, {i + 1, j + 1}};
+                    if (chk(xxs)) {
+                        for (auto&[x, y] : xxs) {
+                            vis[x][y] = 1;
+                        }
+                        continue;
+                    }
 
+                    xxs = {{i, j}, {i + 1, j}, {i + 1, j - 1}};
+                    if (chk(xxs)) {
+                        for (auto&[x, y] : xxs) {
+                            vis[x][y] = 1;
+                        }
+                        continue;
+                    }
+
+                    xxs = {{i, j}, {i, j + 1}, {i + 1, j + 1}};
+                    if (chk(xxs)) {
+                        for (auto&[x, y] : xxs) {
+                            vis[x][y] = 1;
+                        }
+                        continue;
+                    }
+
+                    xxs = {{i, j}, {i, j + 1}, {i + 1, j}};
+                    if (chk(xxs)) {
+                        for (auto&[x, y] : xxs) {
+                            vis[x][y] = 1;
+                        }
+                        continue;
+                    }
+                }
+            }
+        }
+
+        bool ans = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (a[i][j] == '*') {
+                    ans &= vis[i][j];
+                }
+                // cout << vis[i][j] << " ";
+            }
+            // cout << endl;
+        }
+        cout << cq[ans] << endl;
     }
     return 0;
 }
