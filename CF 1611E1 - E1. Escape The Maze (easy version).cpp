b2bdd20553075ed0efd86fe154388f3c2470e82a
MTB Ishmam <134937835+mtbishmam@users.noreply.github.com>
2024-09-28 10:08:15 +0600

    Update CF 1611E1 - E1. Escape The Maze (easy version).cpp

--- CF 1611E1 - E1. Escape The Maze (easy version).cpp
+++ CF 1611E1 - E1. Escape The Maze (easy version).cpp
@@ -1,1 +1,122 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <queue>
+#include <bitset>
+#include <algorithm>
+#include <numeric>
+#include <math.h>
+#include <iomanip>
+#include <cstring>
+// #include <cassert>
+#include <functional>
+using namespace std;
 
+#define endl "\n"
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define lb lower_bound
+#define ub upper_bound
+#define em emplace
+
+template <typename T> istream& operator>>(istream& is, vector<T>& a) { for (auto& i : a) is >> i; return is; }
+template <typename T> ostream& operator<<(ostream& os, vector<T>& a) { for (auto& i : a) os << i << " "; return os; };
+// template <typename T> ostream& operator<<(ostream &os, const vector<T>& v) { os << '{'; string s; for (const auto &x : v) os << s << x, s = " "; return os << '}'; }
+template <typename A, typename B> istream& operator>>(istream& is, pair<A, B>& i) { return is >> i.ff >> i.ss; }
+template <typename A, typename B> ostream& operator<<(ostream& os, const pair<A, B>& i) { return os << i.ff << " " << i.ss; }
+// template <typename A, typename B> ostream& operator<<(ostream& os, const pair<A, B>& i) { return os << '{' << i.ff << ", " << i.ss << '}'; }
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#define debug(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+using ll    = long long      ; using ld    = long double  ; using ull = unsigned ll    ;
+using pii   = pair<int, int> ; using pli  = pair<ll, int> ; using pil = pair<int, ll>  ;
+using pll   = pair<ll, ll>   ; using vl   = vector<ll>    ; using vvl = vector<vl>     ;
+using vi    = vector<int>    ; using vb   = vector<bool>  ; using vc  = vector<char>   ;
+using vvi   = vector<vi>     ; using vvb  = vector<vb>    ; using vvc = vector<vc>     ;
+using vpii  = vector<pii>    ; using vpll = vector<pll>   ; using vs  = vector<string> ;
+using vvpii = vector<vpii>   ; using vvvi = vector<vvi>   ; using vvvl = vector<vvl>   ;
+using tiii = tuple<int, int, int>;                        ; using vtiii = vector<tiii> ;
+template <class A, class B> using umap = unordered_map<A, B>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define bug cerr << "!Bugged..." << endl
+#define add(x, y) (x + y >= MOD ? x + y - MOD : x + y)
+#define sz(x) (int)(x).size()
+
+const string cq[2] = {"NO", "YES"};
+const int dx[8] = { -1,  0, 0, 1, 1,  1, -1, -1};
+const int dy[8] = { 0, -1, 1, 0, 1, -1,  1, -1};
+const int INF = 2147483647;
+const ll LINF = 9223372036854775807;
+const int MOD = 1e9 + 7;
+const int maxn = 1e5 + 1;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // freopen("error.txt", "a", stderr);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    // cout.tie(NULL);
+
+    int T(1);
+    cin >> T;
+    for (int Ti = 1; Ti <= T; Ti++)
+    {
+        int n, k; cin >> n >> k;
+        vi frnds(k); cin >> frnds;
+        vvi g(n + 1);
+        for (int i = 1; i < n; i++) {
+            int x, y;
+            cin >> x >> y;
+            g[x].eb(y);
+            g[y].eb(x);
+        }
+        queue<int> qme;
+        qme.push(1);
+        vi disme(n + 1, INF); disme[1] = 0;
+        while (qme.size()) {
+            int node = qme.front();
+            qme.pop();
+
+            for (auto& child : g[node]) {
+                if (disme[node] + 1 < disme[child]) {
+                    disme[child] = disme[node] + 1;
+                    qme.push(child);
+                }
+            }
+        }
+
+        queue<int> q; vi dis(n + 1, INF);
+        for (auto& i : frnds) q.push(i), dis[i] = 0;
+        // debug(frnds);
+        while (q.size()) {
+            int node = q.front();
+            q.pop();
+            for (auto& child : g[node]) {
+                if (dis[node] + 1 < dis[child]) {
+                    dis[child] = dis[node] + 1;
+                    q.push(child);
+                }
+            }
+        }
+        bool ok = 0;
+        for (int i = 1; i <= n; i++) {
+            if (g[i].size() == 1 and i != 1) {
+                ok |= disme[i] < dis[i];
+            }
+        }
+        // debug(disme[3], dis[3]);
+        cout << cq[ok] << endl;
+    }
+    return 0;
+}
