8fe151e1b7cdf4d76ef18ef51d81125069caafb7
MTB Ishmam <134937835+mtbishmam@users.noreply.github.com>
2024-10-27 16:05:33 +0600

    Update CF 225C - C. Barcode.cpp

--- CF 225C - C. Barcode.cpp
+++ CF 225C - C. Barcode.cpp
@@ -1,1 +1,107 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <queue>
+#include <bitset>
+#include <algorithm>
+#include <numeric>
+#include <math.h>
+#include <iomanip>
+#include <cstring>
+using namespace std;
 
+#define endl "\n"
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define lb lower_bound
+#define ub upper_bound
+#define e emplace
+
+template <typename T> istream& operator>>(istream &is, vector<T>& a) { for (auto& i : a) is >> i; return is; }
+template <typename T> ostream& operator<<(ostream& os, vector<T>& a) { for (auto& i : a) os << i << " "; return os; };
+// template <typename T> ostream& operator<<(ostream &os, const vector<T>& v) { os << '{'; string s; for (const auto &x : v) os << s << x, s = " "; return os << '}'; }
+template <typename A, typename B> istream& operator>>(istream& is, pair<A, B>& i) { return is >> i.ff >> i.ss; }
+template <typename A, typename B> ostream& operator<<(ostream& os, const pair<A, B>& i) { return os << i.ff << " " << i.ss; }
+// template <typename A, typename B> ostream& operator<<(ostream& os, const pair<A, B>& i) { return os << '{' << i.ff << ", " << i.ss << '}'; }
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#define debug(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+using ll    = long long;
+using ld    = long double;
+using pii   = pair<int, int>; using pli  = pair<ll, int>;  using pil = pair<int, ll>;
+using pll   = pair<ll, ll>;   using vl   = vector<ll>;     using vvl = vector<vl>;
+using vi    = vector<int>;    using vb   = vector<bool>;   using vc  = vector<char>;
+using vvi   = vector<vi>;     using vvb  = vector<vb>;     using vvc = vector<vc>;
+using vpii  = vector<pii>;    using vpll = vector<pll>;    using vs  = vector<string>;
+using vvpii = vector<vpii>;   using tiii = tuple<int, int, int>;
+using vtiii = vector<tiii>;
+template <class A, class B> using umap = unordered_map<A, B>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define add(x, y) (x + y >= MOD ? x + y - MOD : x + y)
+
+const int dx[8] = { -1,  0, 0, 1, 1,  1, -1, -1};
+const int dy[8] = { 0, -1, 1, 0, 1, -1,  1, -1};
+const int INF = 2147483647;
+const ll LINF = 9223372036854775807;
+const int MOD = 1e9 + 7;
+const int maxn = 1e5 + 1;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    freopen("error.txt", "a", stderr);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    // cout.tie(NULL);
+
+    int ttt(1);
+    // cin >> ttt;
+    for (int itt = 1; itt <= ttt; itt++)
+    {
+        int n, m, x, y;
+        cin >> n >> m >> x >> y;
+        vvc a(n, vc(m));
+        for (auto& i : a) cin >> i;
+
+        vi cost(m);
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                cost[i] += (a[j][i] == '#');
+        for (int i = 1; i < m; i++)
+            cost[i] += cost[i - 1];
+        // cout << cost << endl;
+
+        ll dp[m][2];
+        memset(dp, -1, sizeof dp);
+        auto get = [&](int i, int j, int k) {
+            int ret = 0, len = j - i + 1;
+            if (!k) ret += cost[j] - (i - 1 < 0 ? 0 : cost[i - 1]);
+            else ret += (len * n) - (cost[j] - (i - 1 < 0 ? 0 : cost[i - 1]));
+            return ret;
+        };
+        function<ll(int, int)> f = [&](int i, int j) -> ll {
+            if (i == m) return 0;
+            ll& ret = dp[i][j];
+            if (~ret) return ret;
+
+            ret = 1e6;
+            for (int cur = x; cur <= y; cur++) {
+                // cout << get(i, i + cur - 1, j) << endl;
+                ret = min(ret, get(i, i + cur - 1, j) + f(i + cur, j ^ 1));
+            }
+            return ret;
+        };
+        cout << min(f(0, 0), f(0, 1));
+    }
+    return 0;
+}
