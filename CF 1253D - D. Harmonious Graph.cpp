3ccc6e80769d3e5a449fbbc9c88dac1287596157
MTB Ishmam <134937835+mtbishmam@users.noreply.github.com>
2024-11-06 10:42:29 +0600

    Update CF 1253D - D. Harmonious Graph.cpp

--- CF 1253D - D. Harmonious Graph.cpp
+++ CF 1253D - D. Harmonious Graph.cpp
@@ -3,116 +3,115 @@
 #include <map>
 #include <set>
 #include <unordered_map>
-#include <unordered_set>
 #include <queue>
-#include <stack>
+#include <bitset>
 #include <algorithm>
 #include <numeric>
 #include <math.h>
 #include <iomanip>
 #include <cstring>
-
-// #pragma GCC optimize("Ofast")
-#pragma GCC target("avx2")
-
-//#include <ext/pb_ds/assoc_container.hpp>
-//#include <ext/pb_ds/tree_policy.hpp>
-//template<typename T>
-//using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-
 using namespace std;
-//using namespace __gnu_pbds;
 
+#define endl "\n"
 #define pb push_back
 #define eb emplace_back
-#define endl "\n"
-#define ll long long
-#define ld long double
-#define pii pair<int, int>
-#define pll pair<ll, ll>
 #define ff first
 #define ss second
+#define lb lower_bound
+#define ub upper_bound
+#define e emplace
 
-template <class T>
-using pq = priority_queue<T>;
-template <class T>
-using pqg = priority_queue<T, vector<T>, greater<T>>;
+template <typename T> istream& operator>>(istream &is, vector<T>& a) { for (auto& i : a) is >> i; return is; }
+template <typename T> ostream& operator<<(ostream& os, vector<T>& a) { for (auto& i : a) os << i << " "; return os; };
+// template <typename T> ostream& operator<<(ostream &os, const vector<T>& v) { os << '{'; string s; for (const auto &x : v) os << s << x, s = " "; return os << '}'; }
+template <typename A, typename B> istream& operator>>(istream& is, pair<A, B>& i) { return is >> i.ff >> i.ss; }
+template <typename A, typename B> ostream& operator<<(ostream& os, const pair<A, B>& i) { return os << i.ff << " " << i.ss; }
+// template <typename A, typename B> ostream& operator<<(ostream& os, const pair<A, B>& i) { return os << '{' << i.ff << ", " << i.ss << '}'; }
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#define debug(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
 
-#define add(a, b) (a + b >= MOD ? a + b - MOD : a + b)
+using ll    = long long;
+using ld    = long double;
+using pii   = pair<int, int>; using pli  = pair<ll, int>;  using pil = pair<int, ll>;
+using pll   = pair<ll, ll>;   using vl   = vector<ll>;     using vvl = vector<vl>;
+using vi    = vector<int>;    using vb   = vector<bool>;   using vc  = vector<char>;
+using vvi   = vector<vi>;     using vvb  = vector<vb>;     using vvc = vector<vc>;
+using vpii  = vector<pii>;    using vpll = vector<pll>;    using vs  = vector<string>;
+using vvpii = vector<vpii>;   using tiii = tuple<int, int, int>;
+using vtiii = vector<tiii>;
+template <class A, class B> using umap = unordered_map<A, B>;
+
 #define all(x) (x).begin(), (x).end()
-#define unique(x) sort(all(x)), (x).erase(unique(all(x)), (x).end())
-#define tolower(x) transform(all(x), (x).begin(), ::tolower)
-#define toupper(x) transform(all(x), (x).begin(), ::toupper)
-#define remove_all(x, y) (x).erase(remove(all(x), char(y)), (x).end());
+#define rall(x) (x).rbegin(), (x).rend()
+#define add(x, y) (x + y >= MOD ? x + y - MOD : x + y)
 
-#define input(in) freopen(in, "r", stdin)
-#define output(out) freopen(out, "w", stdout)
+const int dx[8] = { -1,  0, 0, 1, 1,  1, -1, -1};
+const int dy[8] = { 0, -1, 1, 0, 1, -1,  1, -1};
+const int INF = 2147483647;
+const ll LINF = 9223372036854775807;
+const int MOD = 1e9 + 7;
+const int maxn = 1e5 + 1;
 
-const int INF = 1e9;
-const ll LINF = 1e17;
-// const double EPS = 1e-10;
-// const double gam = 0.5772156649015328606; //euler's constant
-// const double PI = acos(-1.0);
-const int dx[8] = {-1, 0, 0, 1, 1, 1, -1, -1};
-const int dy[8] = {0, -1, 1, 0, 1, -1, 1, -1};
-// const int dxx[8] = {1, 1, 2, 2, -1, -1, -2, -2};
-// const int dyy[8] = {2, -2, 1, -1, 2, -2, 1, -1};
-const int MOD = 1000000007;
-const int maxn = 2e5 + 10;
-//mt19937 rng(chrono::system_clock::now().time_since_epoch().count());
-
-vector<int> par(maxn), mn(maxn), mx(maxn);
-vector<bool> vis(maxn);
-
-int find(int x) { return x == par[x] ? x : par[x] = find(par[x]); }
-
-void unite(int x, int y)
-{
-    x = find(x), y = find(y);
-    if (x != y)
-    {
-        par[x] = y;
-        mn[y] = min(mn[x], mn[y]);
-        mx[y] = max(mx[x], mx[y]);
-    }
-}
-
-void solve()
-{
-    int n, m;
-    cin >> n >> m;
-    for (int i = 1; i <= n; i++)
-        par[i] = mn[i] = mx[i] = i;
-
-    for (int i = 1, x, y; i <= m; i++)
-        cin >> x >> y, unite(x, y);
-
-    int ans(0);
-    for (int i = 1; i <= n; i++)
-    {
-        int x = find(i);
-        if (!vis[x])
-        {
-            vis[x] = 1;
-            for (int j = mn[x]; j <= mx[x]; j++)
-                if (x != find(j))
-                    unite(j, x), ans++;
+struct dsu {
+    int n;
+    vi sz, par;
+    dsu(int _n) : n(_n), sz(n, 1), par(n, -1) {}
+    int find(int x) { return (par[x] == -1 ? x : par[x] = find(par[x])); }
+    int unite(int a, int b) {
+        a = find(a), b = find(b);
+        if (a != b) {
+            if (sz[a] < sz[b]) swap(a, b);
+            par[b] = a;
+            sz[a] += sz[b];
+            return 1;
         }
+        return 0;
     }
-
-    cout << ans;
-}
+};
 
 int main()
 {
+#ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    freopen("error.txt", "a", stderr);
+#endif
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
-    cout.tie(NULL);
+    // cout.tie(NULL);
 
-    int t(1);
-    // cin >> t;
-    while (t--)
-        solve();
+    int ttt(1);
+    // cin >> ttt;
+    for (int itt = 1; itt <= ttt; itt++)
+    {
+        int n, m;
+        cin >> n >> m;
+        dsu ds(n + 5);
+        for (int i = 0, x, y; i < m; i++)
+            cin >> x >> y, ds.unite(x, y);
 
+        vvi g(n + 5);
+        for (int i = 1; i <= n; i++) {
+            int grp = ds.find(i);
+            g[grp].eb(i);
+        }
+        int ans(0);
+        for (int i = 1; i <= n; i++) {
+            vi& b = g[i];
+            sort(all(b));
+            // cout << b << endl;
+            for (int j = 0; j + 1 < b.size(); j++) {
+                if (b[j] + 1 != b[j + 1]) {
+                    for (int k = b[j] + 1; k < b[j + 1]; k++) {
+                        if (ds.unite(b[j], k)) ans++;
+                    }
+
+                }
+            }
+        }
+        cout << ans << endl;
+
+    }
     return 0;
 }
